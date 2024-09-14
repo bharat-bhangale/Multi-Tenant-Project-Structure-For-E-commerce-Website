dc167bbc740d7203908ec7ea0b1d8aa0fb311924
bharat-bhangale <cs.bharat.dev@gmail.com>
2024-09-14 12:21:42 +0530

    Shared folder created for share this layout for accross all tenants

---
+++ src/app/shared/layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const layout = () => {
+  return (
+    <div>
+      Global Layout shared layout accross all tenants
+    </div>
+  )
+}
+
+export default layout
---
+++ src/app/shared/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Landing Page with tenant selection
+    </div>
+  )
+}
+
+export default page
